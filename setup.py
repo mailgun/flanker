7d574d4a128c7a5be02d94d3bf49d7edf659abc4
Russell Jones <rjones@mailgunhq.com>
2013-11-14 20:08:07 -0800

    Bumped version number correctly.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='flanker',
-      version="0.4",
+      version="0.3.1",
       description="Mailgun Parsing Tools",
       long_description='',
       classifiers=[],

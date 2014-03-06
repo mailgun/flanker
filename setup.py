b48de29ac5f4134bc918d5c81cf98eba1a085efd
Russell Jones <rjones@mailgunhq.com>
2014-03-05 16:17:33 -0800

    Updated manifest to include LICENSE file and verison number bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.3.9',
+      version='0.3.10',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

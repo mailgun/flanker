ffa67bff6938c3056d0591782f40641ca9f24688
Russell Jones <rjones@mailgunhq.com>
2014-03-11 12:10:16 -0700

    Fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.2.1',
+      version='0.3.13',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

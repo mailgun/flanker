52b1b2046eb53580539046439ff7446fce12b5c4
Maxim Vladimirsky <horkhe@gmail.com>
2014-08-01 11:58:13 +0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.4.18',
+      version='0.4.19',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

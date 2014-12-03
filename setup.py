3b5d8ba8c497eaa1e0f2ff738aa1759aa99e58a3
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2014-12-02 17:05:27 -0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.4.23',
+      version='0.4.24',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

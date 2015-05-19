10d0df9ce466657d0d0031e5d38079850c23d038
Jesse Spears <jesse@mailgunhq.com>
2015-05-19 12:32:44 -0700

    Removed versions from setup.py for interoperability

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ setup(name='flanker',
           'mock'
       ],
       install_requires=[
-          'chardet>=1.0.1',
-          'cchardet>=0.3.5',
-          'cython>=0.21.1',
-          'dnsq>=1.1.3',
-          'expiringdict>=1.1.2',
-          'WebOb>=0.9.8',
-          'redis>=2.7.1',
+          'chardet',
+          'cchardet',
+          'cython',
+          'dnsq',
+          'expiringdict',
+          'WebOb',
+          'redis',
           # IMPORTANT! Newer regex versions are a lot slower for
           # mime parsing (100x slower) so keep it as-is for now.
           'regex>=0.1.20110315',

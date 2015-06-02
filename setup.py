2c757154c54694de9a2f4bd89b090b34091972e8
Jesse Spears <jesse@mailgunhq.com>
2015-06-02 11:42:30 -0700

    Version bump to v0.4.30

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.4.29',
+      version='0.4.30',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

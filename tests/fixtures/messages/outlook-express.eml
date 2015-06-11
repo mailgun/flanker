mailgun/flanker

commit cd271929bf831999c40e616da3dfacd3694eae56

work harder to avoid re-serializing headers
**Purpose**

We want to avoid re-serializing parsed messages, for two reasons:

1. It's slow. We already have the original text, so if we can output it
   as-is we save a bunch of work.
2. It canonicalizes headers. This can break DKIM signatures, which hurts
   deliverability.

**Implementation**

This commit does three things:

1. `message.headers["some-header"] = "some value"` now causes a prepend
   if "some-header" is not present in the message yet. Prepends are
   better than arbitrary insertions, because they do not force us to
   re-serialize the rest of the message.
2. `message.headers.add("some-header, "some value")` now causes a
   prepend, for the same reason as 1. This previously added the header
   at the end of the list instead. I am assuming this behavior change is OK.
3. `message.body = message.body` no longer causes the message to be
   marked as changed.

Related commit: 9c89b875dc0eafc01c7f9725d5db5aff8f798f70

flanker/mime/message/part.py
@@ -76,8 +76,9 @@ class Stream(object):
                 self.stream.read(self.end - self._body_start + 1))
 
     def _set_body(self, value):
-        self._body = value
-        self._body_changed = True
+        if value != self._body:
+            self._body = value
+            self._body_changed = True
 
 
     def headers_changed(self, ignore_prepends=False):
@@ -479,7 +480,8 @@ class MimePart(RichPartMixin):
         """
         Serializes the message using a file like object.
         """
-        if not self.was_changed():
+        if not self.was_changed(ignore_prepends=True):
+            self.headers.to_stream(out, prepends_only=True)
             out.write(self._container.read_message())
         else:
             try:

flanker/mime/message/headers/headers.py
@@ -34,8 +34,12 @@ class MimeHeaders(object):
         return normalize(key) in self._v
 
     def __setitem__(self, key, value):
-        self._v[normalize(key)] = remove_newlines(value)
-        self.changed = True
+        key = normalize(key)
+        if key in self._v:
+            self._v[key] = remove_newlines(value)
+            self.changed = True
+        else:
+            self.prepend(key, remove_newlines(value))
 
     def __delitem__(self, key):
         del self._v[normalize(key)]
@@ -51,9 +55,7 @@ class MimeHeaders(object):
     def add(self, key, value):
         """Adds header without changing the
         existing headers with same name"""
-
-        self._v.add(normalize(key), remove_newlines(value))
-        self.changed = True
+        self.prepend(key, value)
 
     def keys(self):
         """

tests/mime/message/fallback/fallback_test.py
@@ -227,8 +227,8 @@ def message_headers_mutation_test():
 
 def message_headers_append_test():
     """
-    `prepend` and `add` add a header to the beginning and the end of the header
-    list respectively.
+    `prepend` and `add` both insert a header at the beginning and the end of the
+    header list.
     """
     # Given
     orig = create.from_string(ENCLOSED)
@@ -239,8 +239,8 @@ def message_headers_append_test():
     restored = create.from_python(orig.to_python_message())
 
     # Then
-    eq_(('Foo-Bar', 'hello'), restored.headers.items()[0])
-    eq_(('Blah', 'kitty'), restored.headers.items()[-1])
+    eq_(('Blah', 'kitty'), restored.headers.items()[0])
+    eq_(('Foo-Bar', 'hello'), restored.headers.items()[1])
 
 
 def message_headers_transform_test():

tests/mime/message/headers/headers_test.py
@@ -58,12 +58,12 @@ def headers_multiple_values_test():
     eq_(['5'], h.getall('Mime-Version'))
 
     # use add to add more values
-    h.add('Received', '6')
-    eq_(['2', '4', '6'], h.getall('Received'))
+    h.add('Received', '1')
+    eq_(['1', '2', '4'], h.getall('Received'))
 
     # use prepend to insert header in the begining of the list
     h.prepend('Received', '0')
-    eq_(['0', '2', '4', '6'], h.getall('Received'))
+    eq_(['0', '1', '2', '4'], h.getall('Received'))
 
     # delete removes it all!
     del h['RECEIVED']

tests/mime/message/scanner_test.py
@@ -112,7 +112,7 @@ def mailbox_full_test():
 def test_uservoice_case():
     message = scan(LONG_LINKS)
     html = message.body
-    message.body = html
+    message._container._body_changed = True
     val = message.to_string()
     for line in val.splitlines():
         print line

commit 45e70dc61a8cb979fa16fba1c62fb535fa190670

fix exception in serialization of messages with malformed headers
flanker.mime.create.from_string does not fully parse message headers.
Instead, headers are parsed lazily the first time they are accessed. So
even though flanker.mime.create.from_string does not support malformed
headers, this isn't a problem if they are never accessed.

Commits 9c89b875dc0eafc01c7f9725d5db5aff8f798f70 and
cd271929bf831999c40e616da3dfacd3694eae56 caused headers to be accessed
every time a message is serialized. This made it impossible to parse and
then re-serialize a message with broken headers, even though this used
to be possible if the headers were never accessed after parsing.

This commit brings back that option: it is again possible to parse and
then re-serialize a message with broken headers.

flanker/mime/message/part.py
@@ -80,6 +80,9 @@ class Stream(object):
             self._body = value
             self._body_changed = True
 
+    def _stream_prepended_headers(self, out):
+        if self._headers:
+            self._headers.to_stream(out, prepends_only=True)
 
     def headers_changed(self, ignore_prepends=False):
         return self._headers is not None and self._headers.have_changed(ignore_prepends)
@@ -179,6 +182,9 @@ class Body(object):
     def body_changed(self):
         return True
 
+    def _stream_prepended_headers(self, out):
+        self.headers.to_stream(out, prepends_only=True)
+
 
 class Part(object):
 
@@ -198,6 +204,9 @@ class Part(object):
 
     def body_changed(self):
         return True
+
+    def _stream_prepended_headers(self, out):
+        self.headers.to_stream(out, prepends_only=True)
 
 
 class RichPartMixin(object):
@@ -469,7 +478,7 @@ class MimePart(RichPartMixin):
         # no copying, no alternation, yeah!
         if self.is_root() and not self.was_changed(ignore_prepends=True):
             with closing(StringIO()) as out:
-                self.headers.to_stream(out, prepends_only=True)
+                self._container._stream_prepended_headers(out)
                 return out.getvalue() + self._container.string
         else:
             with closing(StringIO()) as out:
@@ -481,7 +490,7 @@ class MimePart(RichPartMixin):
         Serializes the message using a file like object.
         """
         if not self.was_changed(ignore_prepends=True):
-            self.headers.to_stream(out, prepends_only=True)
+            self._container._stream_prepended_headers(out)
             out.write(self._container.read_message())
         else:
             try:

tests/__init__.py
@@ -65,6 +65,7 @@ QUOTED_PRINTABLE = open(
 TEXT_ONLY = open(fixture_file("messages/text-only.eml")).read()
 MAILGUN_PIC = open(fixture_file("messages/mailgun-pic.eml")).read()
 BZ2_ATTACHMENT  = open(fixture_file("messages/bz2-attachment.eml")).read()
+OUTLOOK_EXPRESS = open(fixture_file("messages/outlook-express.eml")).read()
 
 AOL_FBL = open(fixture_file("messages/complaints/aol.eml")).read()
 YAHOO_FBL = open(fixture_file("messages/complaints/yahoo.eml")).read()

tests/fixtures/messages/outlook-express.eml
@@ -1,0 +1,28 @@
+X-Envelope-From: <bob@example.com>
+Received: from hwmail1.mailset.cn (hwmail1.mailset.cn [106.186.124.194]) by
+ mxa.mailgun.org with ESMTP id 53c3560e.7fe658457ed8-in3; Mon, 14 Jul 2014
+ 04:01:18 -0000 (UTC)
+Received: (ce send program); Mon, 14 Jul 2014 12:01:03 +0800
+Received: from 121.14.7.230 (HELO etcsmtp.xinnetdns.com) (121.14.7.230) by
+ 106.186.124.194 with SMTP; Mon, 14 Jul 2014 12:01:03 +0800
+Received: (ce send program); Mon, 14 Jul 2014 11:49:09 +0800
+X-Ce-Autoforward: etcsmtp.xinnetdns.com
+Message-Id: <EFOOJNZYKZFPXKXDZPRSCFMBVCHB.bob@example.com>
+Received: (ce send program); Mon, 14 Jul 2014 11:49:07 +0800
+Received: from 123.100.1.157 (HELO sink-gdmm.com) (123.100.1.157) by
+ 121.14.68.74 with SMTP; Mon, 14 Jul 2014 11:49:07 +0800
+Return-Path: <bob@example.com>
+Received: from [110.230.143.217] by 121.14.68.114 with surfront esmtp id
+ 9885528886420;	Mon, 14 Jul 2014 12:00:56 +0800 (CST)
+From: Bobby <bob@example.com>
+Subject: Sup?
+To: "Ally" <ally@example.com>
+Content-Type: application/octet-stream;
+        name="�½� �ı��ĵ�.txt"
+Content-Transfer-Encoding: base64
+Mime-Version: 1.0
+Date: Tue, 14 Jul 2015 12:19:08 +0800
+X-Mailer: Microsoft Outlook Express 6.00.2800.1106
+X-Mailgun-Incoming: Yes
+
+0YLQtdC60YHRgtC+0LLQvtC1INCy0LvQvtC20LXQvdC40LU=

tests/mime/message/part_test.py
@@ -14,7 +14,7 @@ from tests import (BILINGUAL, BZ2_ATTACHMENT, ENCLOSED, TORTURE, TORTURE_PART,
                    TEXT_ONLY, ENCLOSED_BROKEN_BODY, RUSSIAN_ATTACH_YAHOO,
                    MAILGUN_PIC, MAILGUN_PNG, MULTIPART, IPHONE,
                    SPAM_BROKEN_CTYPE, BOUNCE, NDN, NO_CTYPE, RELATIVE,
-                   MULTI_RECEIVED_HEADERS)
+                   MULTI_RECEIVED_HEADERS, OUTLOOK_EXPRESS)
 from tests.mime.message.scanner_test import TORTURE_PARTS, tree_to_string
 
 
@@ -234,6 +234,16 @@ def preserve_formatting_with_new_headers_test():
     new_header, remaining_mime = message.to_string().split('\r\n', 1)
     eq_('X-New-Header: Added', new_header)
     eq_(MULTIPART, remaining_mime)
+
+
+def parse_then_serialize_malformed_message_test():
+    """
+    We don't have to fully parse message headers if they are never accessed.
+    Thus we should be able to parse then serialize a message with malformed
+    headers without crashing, even though we would crash if we fully parsed it.
+    """
+    serialized = scan(OUTLOOK_EXPRESS).to_string()
+    eq_(OUTLOOK_EXPRESS, serialized)
 
 
 def ascii_to_unicode_test():
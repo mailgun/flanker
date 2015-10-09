a5868610125b725900710c1d86e5aff1d51115fb
Russell Jones <rjones@mailgunhq.com>
2015-10-09 20:07:39 +0000

    Version bump for release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.4.36',
+      version='0.4.37',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

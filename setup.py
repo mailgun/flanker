b1210c8875224db7e04a58be97c0b9a8e8bfcc83
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2017-03-29 11:51:01 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.4.55',
+      version='0.5.0-rc1',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

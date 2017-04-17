a881cd0c7ec47c01bb1abbff587e80f521498ce3
Maxim Vladimirskiy <horkhe@gmail.com>
2017-04-17 10:53:00 -0700

    Fix UnicodeDecodeError in logging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.6.3',
+      version='0.6.4',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],

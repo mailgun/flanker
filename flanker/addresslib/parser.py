17bacd74897c529e7ff9de9b3530876a24cdb8c4
frankobe <mua08p@gmail.com>
2017-06-09 14:29:33 -0700

    add outputdir for yacc parser generation to avoid permission denied

--- flanker/addresslib/parser.py
+++ flanker/addresslib/parser.py
@@ -159,23 +159,23 @@ def p_error(p):
 
 log.info('building mailbox parser')
 mailbox_parser = yacc.yacc(
-    start='mailbox', errorlog=log)
+    start='mailbox', errorlog=log, outputdir='/etc/inboxapp/')
 
 log.info('building addr_spec parser')
 addr_spec_parser = yacc.yacc(
-    start='addr_spec', errorlog=log)
+    start='addr_spec', errorlog=log, outputdir='/etc/inboxapp/')
 
 log.info('building url parser')
 url_parser = yacc.yacc(
-    start='url', errorlog=log)
+    start='url', errorlog=log, outputdir='/etc/inboxapp/')
 
 log.info('building mailbox_or_url parser')
 mailbox_or_url_parser = yacc.yacc(
-    start='mailbox_or_url', errorlog=log)
+    start='mailbox_or_url', errorlog=log, outputdir='/etc/inboxapp/')
 
 log.info('building mailbox_or_url_list parser')
 mailbox_or_url_list_parser = yacc.yacc(
-    start='mailbox_or_url_list', errorlog=log)
+    start='mailbox_or_url_list', errorlog=log, outputdir='/etc/inboxapp/')
 
 
 # Interactive prompt for easy debugging

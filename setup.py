a4501945d4d7895eb995628939dc91613e88aea0
Maxim Vladimirskiy <horkhe@gmail.com>
2018-05-17 09:17:09 +0300

    Fix setup.py after bad merge

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(name='flanker',
-      version='0.8.6',
+      version='0.9.1',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
-          'Topic :: Software Development :: Libraries',
-          'License :: OSI Approved :: Apache 2',
+          'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
       ],
       keywords='',
       author='Mailgun Technologies Inc.',

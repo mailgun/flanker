a3c403e270130e906571b03237e144c904f065d2
aerwin3 <aerwin@mailgun.com>
2018-10-26 10:28:43 -0500

    Adding outlook.com to corrector

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = [
 ],
 
 setup(name='flanker',
-      version='0.9.5',
+      version='0.9.6',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[

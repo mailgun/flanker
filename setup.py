1c814ec92af02db1b0c69d0a3a5697db4c64db30
Chris Farmer <zerreissen@hotmail.com>
2018-11-05 14:22:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = [
 ],
 
 setup(name='flanker',
-      version='0.9.6',
+      version='0.9.7',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[
